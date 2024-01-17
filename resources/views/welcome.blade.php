54d469184234b5752fa172a8a6461be438fd30e1
mohammad <mohammad@inktweb.nl>
2024-01-17 12:29:00 +0100

    add customized home page background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="author" content="Mohammad Aldaas">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="Profile, developer, Laravel, PHP, HTML, CSS, JavaScript">
+        <meta name="Description" content="Discover more about top-notch full-stack developers. Enjoy the journey! ">
 
-        <title>Laravel</title>
+        <title>Mohammad Aldaas - profile</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="antialiased">
-
+    <body>
+        <canvas id="myCanvas"></canvas>
     </body>
 </html>
