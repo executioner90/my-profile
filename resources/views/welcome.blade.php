9f007e2097b291cb99e892661605b2aa2c5210a7
mohammad <mohammad@inktweb.nl>
2024-01-18 09:29:59 +0100

    Revert "add customized home page background"
    This reverts commit 36be0b47c6f58f88d78b3ee2ef2a5a9063dbdf44.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,20 +12,5 @@
     </head>
     <body>
         <canvas id="myCanvas"></canvas>
-        <div class="flex flex-col h-screen justify-center items-center">
-            <div class="text-white text-lg md:text-4xl mb-5">
-                Hello, I'm <span class="highlight">Mohammad Aldaas</span>.
-                <br>
-                I'm a full-stack web developer.
-                <br>
-                <span class="slogan">Crafting Code, Building Dreams: Your Vision, Our Expertise!</span>
-            </div>
-
-            <div class="bg-transparent button hover:text-white py-2 px-4 border rounded hover:show-down">
-                View my work
-                <i class="fa fa-arrow-right text-base pl-2"></i>
-                <i class="fa fa-arrow-down text-base pl-2"></i>
-            </div>
-        </div>
     </body>
 </html>
