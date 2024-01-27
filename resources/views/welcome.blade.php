35dd67187885d48ac0daa55c9d90222354089858
mohammad <mohammad@inktweb.nl>
2024-01-27 13:56:59 +0100

    added styling to about page (v1)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,118 +77,111 @@
 
         {{-- About me --}}
         <div id="about" class="min-h-screen w-screen about">
-            <div class="grid grid-cols-4 gap-10">
-                <div class="about-me-card">
+            <div class="grid grid-cols-4 gap-20 mt-1.5 mb-2">
+                <div class="about-me-card py-10 px-3 flex items-center justify-center text-center rounded-3xl">
                     Last station The Netherlands
                 </div>
 
-                <div class="about-me-card">
+                <div class="about-me-card py-10 px-3 flex items-center justify-center text-center rounded-3xl">
                     Embarking on a journey to Europe (Germany one year)
                 </div>
 
-                <div class="about-me-card">
+                <div class="about-me-card py-10 px-3 flex items-center justify-center text-center rounded-3xl">
                     Hard work in Lebanon
                 </div>
 
-                <div class="about-me-card">
+                <div class="about-me-card py-10 px-3 flex items-center justify-center text-center rounded-3xl">
                     Embarking on a journey to Lebanon
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-10">
-                <div class="about-me-card">
+            <div class="grid grid-cols-4 gap-20 my-3">
+                <div class="about-me-card py-8 px-3 flex items-center justify-center text-center rounded-3xl">
                     Fulltime study and working simultaneously
                 </div>
+
                 <div class="col-span-2">
-                    <div class="grid grid-cols-3">
-                        <div class="about-me-card">
-                            - Flexibility
-                            - Understand more about individuality
+                    <div class="grid grid-cols-2 m-3">
+                        <div class="about-me-card py-8 px-6 flex items-center justify-center text-start rounded-3xl mr-2">
+                            Flexibility and more understanding about individuality
                         </div>
-                        <div></div>
-                        <div class="about-me-card">
-                            - Stay positive (except when it comes to corona)
-                            - Work under pressure
+                        <div class="about-me-card py-8 px-6 flex items-center justify-center rounded-3xl">
+                            Work under pressure and stay positive (except when it comes to corona)
                         </div>
                     </div>
                 </div>
 
-                <div class="about-me-card">
-                    War
+                <div class="about-me-card py-8 px-3 flex items-center justify-center text-center rounded-3xl">
+                    The catastrophe (Syria 2011)
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-10">
-                <div class="about-me-card">
-                    High school certificates
-                    - English
-                    - Maths
-                    - Physics
+            <div class="grid grid-cols-4 gap-20 my-3">
+                <div class="about-me-card py-1 px-6 flex items-center justify-center text-center rounded-3xl">
+                    High school certificates (English, maths, physics)
                 </div>
 
                 <div class="col-span-2">
-                    <div class="grid grid-cols-3">
-                        <div class="about-me-card">
-                            - Never give up
-                            - Patience acquired
+                    <div class="grid grid-cols-3 m-3">
+                        <div class="about-me-card py-1 px-3 flex items-center justify-center rounded-3xl">
+                            Never give up and be patience.
                         </div>
 
-                        <div class="about-me-card">
-                            photo of me now!
+                        <div class="px-3 flex items-center justify-center text-center rounded-3xl">
+                            <div class="w-4/6">
+                                <img src="{{ asset('/images/now-me.png') }}" alt="Me now">
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="about-me-card">
+                <div class="about-me-card py-1 px-3 flex items-center justify-center text-center rounded-3xl">
                     Highs school education in Syria
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-10">
-                <div class="about-me-special-card">
-                    College Diploma Software developer
+            <div class="grid grid-cols-4 gap-20 my-3">
+                <div class="about-me-special-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
+                    Diploma Software developer
                 </div>
 
                 <div class="col-span-2">
-                    <div class="grid grid-cols-3">
-                        <div class="about-me-card">
-                            - Analytical skills
-                            - Practicality
-                            - Teamwork
+                    <div class="grid grid-cols-2 rounded-3xl m-3">
+                        <div class="about-me-card py-3 px-6 flex items-center justify-center rounded-3xl mr-2">
+                            Practicality, analytical skills and teamwork
                         </div>
 
-                        <div></div>
-
-                        <div class="about-me-card">
-                            - Dedication
-                            - Commitment
+                        <div class="about-me-card py-3 px-6 flex items-center justify-center text-start rounded-3xl">
+                            Dedication and commitment
                         </div>
                     </div>
                 </div>
 
 
-                <div class="about-me-special-card">
+                <div class="about-me-special-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
                     From childhood till now, my dream has been a constant companion, always alive and burning bright - to be a software developer.
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-10">
-                <div class="about-me-card">
+            <div class="grid grid-cols-4 gap-20 my-3">
+                <div class="about-me-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
                     A software developer for <a href="https://www.inktweb.nl">Inktweb.nl</a>
                 </div>
 
                 <div class="col-span-2">
-                    <div class="grid grid-cols-3">
+                    <div class="grid grid-cols-3 my-2">
                         <div></div>
-                        <div class="about-me-special-card">
+                        <div class="about-me-special-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
                             Dream achieved!
                         </div>
                         <div></div>
                     </div>
                 </div>
 
-                <div class="about-me-card">
-                    Personal photo when I was kid
+                <div class="py-3 px-3 flex items-center justify-center text-center rounded-3xl">
+                    <div class="w-2/6">
+                        <img src="{{ asset('/images/young-me.png') }}" alt="Young me">
+                    </div>
                 </div>
             </div>
         </div>
