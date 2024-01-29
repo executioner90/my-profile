3ac2cd7c6c5641bc1dbd59e12a350be94c5ecad9
mohammad <mohammad@inktweb.nl>
2024-01-29 10:18:25 +0100

    finished about styling.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
         {{-- About me --}}
         <div id="about" class="min-h-screen w-screen about">
-            <div class="grid grid-cols-4 gap-20 mt-1.5 mb-2">
+            <div class="grid grid-cols-4 gap-20 mt-3.5 mb-2">
                 <div class="about-me-card py-10 px-3 flex items-center justify-center text-center rounded-3xl">
                     Last station The Netherlands
                 </div>
@@ -95,63 +95,67 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-20 my-3">
-                <div class="about-me-card py-8 px-3 flex items-center justify-center text-center rounded-3xl">
+            <div class="grid grid-cols-4 gap-20 my-6">
+                <div class="about-me-card py-6 px-3 flex items-center justify-center text-center rounded-3xl">
                     Fulltime study and working simultaneously
                 </div>
 
                 <div class="col-span-2">
                     <div class="grid grid-cols-2 m-3">
-                        <div class="about-me-card py-8 px-6 flex items-center justify-center text-start rounded-3xl mr-2">
+                        <div class="about-me-card py-6 px-6 flex items-center justify-center text-center rounded-3xl mr-2">
                             Flexibility and more understanding about individuality
                         </div>
-                        <div class="about-me-card py-8 px-6 flex items-center justify-center rounded-3xl">
+                        <div class="about-me-card py-6 px-6 flex items-center justify-center text-center rounded-3xl">
                             Work under pressure and stay positive (except when it comes to corona)
                         </div>
                     </div>
                 </div>
 
-                <div class="about-me-card py-8 px-3 flex items-center justify-center text-center rounded-3xl">
+                <div class="about-me-card py-6 px-3 flex items-center justify-center text-center rounded-3xl">
                     The catastrophe (Syria 2011)
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-20 my-3">
-                <div class="about-me-card py-1 px-6 flex items-center justify-center text-center rounded-3xl">
+            <div class="grid grid-cols-4 gap-20 my-6">
+                <div class="about-me-card py-2 px-3 flex items-center justify-center text-center rounded-3xl">
                     High school certificates (English, maths, physics)
                 </div>
 
                 <div class="col-span-2">
                     <div class="grid grid-cols-3 m-3">
-                        <div class="about-me-card py-1 px-3 flex items-center justify-center rounded-3xl">
-                            Never give up and be patience.
+                        <div class="about-me-card py-2 px-6 flex items-center justify-center text-center rounded-3xl">
+                            Never give up and be patient
                         </div>
 
-                        <div class="px-3 flex items-center justify-center text-center rounded-3xl">
+                        <div class="flex items-center justify-center text-center rounded-3xl">
                             <div class="w-4/6">
                                 <img src="{{ asset('/images/now-me.png') }}" alt="Me now">
                             </div>
                         </div>
+
+                        <div class="about-me-card py-2 px-6 flex items-center justify-center text-center rounded-3xl">
+                            Ambition
+                        </div>
                     </div>
                 </div>
 
-                <div class="about-me-card py-1 px-3 flex items-center justify-center text-center rounded-3xl">
+                <div class="about-me-card py-2 px-3 flex items-center justify-center text-center rounded-3xl">
                     Highs school education in Syria
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-20 my-3">
+            <div class="grid grid-cols-4 gap-20 my-6">
                 <div class="about-me-special-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
                     Diploma Software developer
                 </div>
 
                 <div class="col-span-2">
                     <div class="grid grid-cols-2 rounded-3xl m-3">
-                        <div class="about-me-card py-3 px-6 flex items-center justify-center rounded-3xl mr-2">
+                        <div class="about-me-card py-8 px-6 flex items-center justify-center text-center rounded-3xl mr-2">
                             Practicality, analytical skills and teamwork
                         </div>
 
-                        <div class="about-me-card py-3 px-6 flex items-center justify-center text-start rounded-3xl">
+                        <div class="about-me-card py-8 px-6 flex items-center justify-center text-center rounded-3xl">
                             Dedication and commitment
                         </div>
                     </div>
@@ -163,16 +167,19 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-20 my-3">
+            <div class="grid grid-cols-4 gap-20 mt-6">
                 <div class="about-me-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
-                    A software developer for <a href="https://www.inktweb.nl">Inktweb.nl</a>
+                    <span>
+                        A software developer for <a class="hover:text-green-400" href="https://www.inktweb.nl">Inktweb.nl</a>
+                    </span>
                 </div>
 
-                <div class="col-span-2">
-                    <div class="grid grid-cols-3 my-2">
-                        <div></div>
-                        <div class="about-me-special-card py-3 px-3 flex items-center justify-center text-center rounded-3xl">
-                            Dream achieved!
+                <div class="col-span-2 flex items-center justify-center">
+                    <div class="grid grid-cols-1">
+                        <div class="text-center">
+                            <div class="about-me-special-card py-6 px-6 rounded-3xl">
+                                Dream achieved!
+                            </div>
                         </div>
                         <div></div>
                     </div>
