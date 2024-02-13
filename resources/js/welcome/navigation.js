35741e18a20263f469f548f2c7b528a9e3037d25
mohammad <mohammad@inktweb.nl>
2024-02-13 11:41:01 +0100

    clean navigation js file

--- resources/js/welcome/navigation.js
+++ resources/js/welcome/navigation.js
@@ -5,43 +5,6 @@ const navbarLinks = document.getElementById('navbar-links');
 const menuClose =  document.getElementById('close-icon');
 let lastClickedSection = null;
 
-// Focus on nav items when user scrolling manually.
-window.addEventListener("scroll", function() {
-    let scrollPosition = window.scrollY;
-    let sections = document.querySelectorAll(".section");
-    let id = '';
-
-    for (let i = 0; i < sections.length; i++) {
-        let div = sections[i];
-        let offsetTop = div.offsetTop - 350;
-        let offsetBottom = offsetTop + div.offsetHeight;
-
-        if (offsetTop <= scrollPosition && offsetBottom >= scrollPosition) {
-            switch (div.id) {
-                case 'home':
-                    id = 'homeLink';
-                    break;
-                case 'about':
-                    id = 'aboutLink';
-                    break;
-                case 'projects':
-                    id = 'projectsLink';
-                    break;
-                case 'contact':
-                    id = 'contactLink';
-                    break;
-            }
-        }
-    }
-
-    focused(document.getElementById(id));
-
-    // Check if the last clicked section is currently not in view
-    if (lastClickedSection && !isElementInViewport(document.getElementById(lastClickedSection))) {
-        lastClickedSection = null;
-    }
-});
-
 // Click event on nav items.
 document.querySelectorAll('.nav-item').forEach(item => (
     item.addEventListener('click', (event) => {
@@ -66,6 +29,12 @@ document.querySelectorAll('.nav-item').forEach(item => (
                 navigate('contact', mobile);
                 break;
         }
+
+        if (navItem.id === 'mainNav') {
+            focused(document.getElementById('aboutLink'));
+        } else {
+            focused(navItem);
+        }
     }))
 );
 
@@ -73,18 +42,6 @@ toggleMobileNavBtn.addEventListener('click', () => {
     toggleMenuButton();
     toggleMenu();
 });
-
-function focused(element) {
-    // Reset the styles of all navigation links
-    document.querySelectorAll('.navbar div').forEach(link => {
-        link.classList.remove(className);
-    });
-
-    // Apply styles to the focused link
-    if (element) {
-        element.classList.add(className);
-    }
-}
 
 function navigate(section, mobile = true) {
     if (mobile) {
@@ -98,6 +55,20 @@ function navigate(section, mobile = true) {
     }
 }
 
+function focused(element) {
+    console.log(element);
+    // Reset the styles of all navigation links
+    document.querySelectorAll('.navbar div').forEach(link => {
+        link.classList.remove(className);
+    });
+
+    // Apply styles to the focused link
+    if (element) {
+        console.log(element.id);
+        element.classList.add(className);
+    }
+}
+
 function toggleMenuButton() {
     menuOpen.classList.toggle('hidden');
     menuClose.classList.toggle('hidden');
@@ -107,12 +78,3 @@ function toggleMenu() {
     navbarLinks.classList.toggle('hidden');
 }
 
-function isElementInViewport(el) {
-    let rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
