1bccc889a0be1aa56e5822d0f114a2ed02e39f1f
mohammad <mohammad@inktweb.nl>
2024-02-15 11:28:36 +0100

    allow scrolling on large screens too and disable it only when navigating.

--- resources/js/welcome/navigation.js
+++ resources/js/welcome/navigation.js
@@ -6,6 +6,35 @@ const menuClose =  document.getElementById('close-icon');
 let lastClickedSection = null;
 
 window.addEventListener("scroll", function() {
+    let scrollPosition = window.scrollY;
+    let sections = document.querySelectorAll(".section");
+    let id = '';
+
+    for (let i = 0; i < sections.length; i++) {
+        let div = sections[i];
+        let offsetTop = div.offsetTop - 350;
+        let offsetBottom = offsetTop + div.offsetHeight;
+
+        if (offsetTop <= scrollPosition && offsetBottom >= scrollPosition) {
+            switch (div.id) {
+                case 'home':
+                    id = 'homeLink';
+                    break;
+                case 'about':
+                    id = 'aboutLink';
+                    break;
+                case 'projects':
+                    id = 'projectsLink';
+                    break;
+                case 'contact':
+                    id = 'contactLink';
+                    break;
+            }
+        }
+    }
+
+    focused(document.getElementById(id));
+
     // Check if the last clicked section is currently not in view
     if (lastClickedSection && !isElementInViewport(document.getElementById(lastClickedSection))) {
         // reset last clicked section
@@ -52,6 +81,8 @@ toggleMobileNavBtn.addEventListener('click', () => {
 });
 
 function navigate(section, mobile = true) {
+    // prevent double-clicking before navigating finish
+    document.querySelector('html').style.overflow = 'hidden';
     if (mobile) {
         toggleMenuButton();
         toggleMenu();
@@ -61,6 +92,11 @@ function navigate(section, mobile = true) {
         document.getElementById(section).scrollIntoView();
         lastClickedSection = section;
     }
+
+    // Allow scrolling/clicking again
+    setTimeout(() => {
+        document.querySelector('html').style.overflow = 'auto'
+    }, 1000);
 }
 
 function focused(element) {
